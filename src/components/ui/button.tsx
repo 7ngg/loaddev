03fdde229943851651d49411658155aecc91a689
7ngg <m.farm4nov@gmail.com>
2025-04-25 02:03:09 +0400

    feat: simple button

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export default function Button({ children, className, ...props }: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={cn(
+        className,
+        "w-24 h-8 outline shadow rounded cursor-pointer bg-[var(--foreground)] text-[var(--background)] active:scale-95 hover:opacity-90",
+      )}
+    >
+      {children}
+    </button>
+  );
+}

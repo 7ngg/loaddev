023096d99b9d5d380f301328a63a4aadc329d6d5
7ngg <m.farm4nov@gmail.com>
2025-04-25 20:28:56 +0400

    feat(auth): sign up form

--- src/app/auth/schemas.ts
+++ src/app/auth/schemas.ts
@@ -9,4 +9,23 @@ export const signInSchema = z.object({
     .min(8, { message: "Password should be at least 8 characters long" }),
 });
 
+export const signUpSchema = z
+  .object({
+    username: z.string().min(3, {
+      message: "Username should be at least 3 characters long",
+    }),
+    email: z.string().email("Invalid email format"),
+    password: z
+      .string()
+      .min(8, { message: "Password should be at least 8 characters long" }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Password should be at least 8 characters long" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password do not match",
+    path: ["confirmPassword"],
+  });
+
 export type SignInSchemaType = z.infer<typeof signInSchema>;
+export type SignUpSchemaType = z.infer<typeof signUpSchema>;

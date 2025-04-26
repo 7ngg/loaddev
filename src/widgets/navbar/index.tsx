f6da193b9f41cc6976619c82aca729e3c84b4eae
7ngg <m.farm4nov@gmail.com>
2025-04-27 03:06:21 +0400

    fix(navbar): item hover

--- src/widgets/navbar/index.tsx
+++ src/widgets/navbar/index.tsx
@@ -1,27 +1,60 @@
+"use client";
+
 import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
 import { navItems } from "./items";
 import ModeToggle from "./mode-toggle";
+import { useEffect, useRef, useState } from "react";
 
 export default function Navbar() {
+  const [selected, setSelected] = useState<number | null>(null);
+  const underlineRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (selected === null) {
+      underlineRef.current!.style.opacity = "0";
+    }
+
+    const item = document.getElementById(`underline-pos-${selected}`);
+
+    if (!item) return;
+
+    const itemRect = item.getBoundingClientRect();
+    const { left } = item.parentElement!.getBoundingClientRect();
+
+    underlineRef.current!.style.transform = `translateX(${itemRect.left - left}px)`;
+    underlineRef.current!.style.width = `${itemRect.width}px`;
+    underlineRef.current!.style.opacity = "1";
+  }, [selected]);
+
   return (
     <div className="z-10 h-12 min-w-screen fixed flex items-center justify-between px-8 my-2 font-bold text-xl">
       <Image src="/logo.png" alt="logo" width={200} height={100} priority />
-      <div className="flex gap-8  h-full bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-transparent bg-clip-text">
-        {navItems.map((item, index) => {
-          return (
-            <Link
-              href={item.relativePath}
-              key={index}
-              className="h-full flex items-center px-2 hover:bg-white duration-500 rounded"
-            >
-              {item.name}
-            </Link>
-          );
-        })}
+
+      <div
+        className="relative flex gap-8 h-full"
+        onMouseLeave={() => setSelected(null)}
+      >
+        <div
+          ref={underlineRef}
+          className="absolute bottom-0 h-1 bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 transition-all duration-300 rounded-full opacity-0"
+        />
+
+        {navItems.map((item, index) => (
+          <Link
+            id={`underline-pos-${index}`}
+            href={item.relativePath}
+            key={index}
+            onMouseEnter={() => setSelected(index)}
+            className="h-full flex items-center px-2 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500"
+          >
+            {item.name}
+          </Link>
+        ))}
       </div>
+
       <div className="flex gap-4 items-center">
         <ModeToggle />
         <Link href={"/auth"}>
